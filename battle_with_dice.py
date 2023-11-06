0ec0721ebb97a75dc4ef5120fe92a07a6a2e712e
Nayr91 <ryanabc91@gmail.com>
2023-11-06 23:42:42 +0000

    Final implementation of special attacks

--- battle_with_dice.py
+++ battle_with_dice.py
@@ -1,6 +1,14 @@
 import random
 import time
 
+#Declaring different dice
+d20dice = range(1, 20)
+d12dice = range(1, 12)
+d10dice = range(1, 10)
+d8dice = range(1, 8)
+d6dice = range(1, 6)
+
+
 # Declare classes
 class Character:
     def __init__ (self, name, health, atkbonus, armcla, basedmg, dicedmg, atkdice):
@@ -11,6 +19,7 @@ class Character:
         self.basedmg = basedmg
         self.dicedmg = dicedmg
         self.atkdice = atkdice
+        self.goldvalue = health
     def __repr__(self):
       intro = "{name} has {health} health with an armour class of {armcla}".format(name = self.name, health = self.health, armcla = self.armcla)
       return intro
@@ -20,35 +29,61 @@ class Character:
       return self.health
     
     def atk_roll(self):
-      dice_roll = d20dice + self.atkbonus
+      dice_roll = random.choice(d20dice) + self.atkbonus
       return dice_roll
 
     def deal_damage(self):
-      damage = self.basedmg + (self.dicedmg * self.atkdice)
+      damage = self.basedmg + (self.dicedmg * (random.choice(self.atkdice)))
       return damage
     
     def gold_value(self):
-      value = random.randint(40, self.health)
+      value = random.randint(40, self.goldvalue)
       value = value * 3
       return value
 
 class Warrior(Character):
-  def __init__(self, name):
-    Character.__init__(self, name, 200, 6, 14, 10, 3, d10dice)
+    def __init__(self, name):
+      Character.__init__(self, name, 200, 6, 14, 10, 3, d10dice)
+      
+      self.charges = 2
+      
+    def special_attack(self):
+      if self.charges <= 0:
+        return 0
+      print("You use RECKLESS STRIKES")
+      output = random.choice(d20dice) * 3 + 10
+      return output
 
 class Rogue(Character):
-  def __init__(self, name):
-    Character.__init__(self, name, 120, 8, 16, 5, 5, d6dice)
+    def __init__(self, name):
+      Character.__init__(self, name, 120, 8, 16, 5, 5, d6dice)
+      
+      self.charges = 3
+      
+    def special_attack(self):
+      if self.charges <= 0:
+        return 0
+      print("You use SNEAK ATTACK")
+      output = random.choice(d12dice) * 5 + 8
+      self.charges -= 1
+      return output
 
 class Mage(Character):
-  def __init__(self, name):
-    Character.__init__(self, name, 80, 10, 16, 10, 3, d12dice)
+    def __init__(self, name):
+      Character.__init__(self, name, 80, 10, 16, 10, 3, d12dice)
+      
+      self.charges = 4
+      
+    def special_attack(self):
+      if self.charges <= 0:
+        return 0
+      print("You use FIREBALL")
+      time.sleep(1)
+      output = random.choice(d8dice) * 8 + 6
+      self.charges -= 1
+      return output
 
-d20dice = random.choice(range(1, 20))
-d12dice = random.choice(range(1, 12))
-d10dice = random.choice(range(1, 10))
-d8dice = random.choice(range(1, 8))
-d6dice = random.choice(range(1, 6))
+
 
 def enemy_choice(x):
   if x == 1:
@@ -96,8 +131,8 @@ def start():
   input("Press Enter to roll...\n")
   player_roll, baddie_roll = 0, 0
   while player_roll == baddie_roll:
-    player_roll = d20dice
-    baddie_roll = d20dice
+    player_roll = random.choice(d20dice)
+    baddie_roll = random.choice(d20dice)
     print(f"Player rolled: {player_roll}")
     time.sleep(1)
     print(f"{opponent.name} rolled {baddie_roll}")
@@ -118,21 +153,43 @@ def player_turn(player, opponent):
     time.sleep(1)
     print("Time to attack!")
     time.sleep(1)
-    input("Press Enter to attack...\n")
-    time.sleep(1)
-    attackroll = player.atk_roll()
-    print(f"You rolled {attackroll} to strike again {opponent.name} armour class of {opponent.armcla}!")
-    if attackroll >= opponent.armcla:
-      damagedealt = player.deal_damage()
-      opponent.take_damage(damagedealt)
-      print(f"You dealt {damagedealt} to {opponent.name}")
-      print(f"{opponent.name} has {opponent.health} HP left")
-      time.sleep(1)
-      baddie_turn(player, opponent)
+    if player.charges > 0:
+      style = int(input("Type 1 for normal attack, or 2 for Special attack\n"))
+      if style == 2:
+        player.charges -= 1
+        print(f"You have {player.charges} charges remaining of your special attack")
+        attackroll = player.atk_roll()
+        print(f"You rolled {attackroll} to strike again {opponent.name} armour class of {opponent.armcla}!")
+        if attackroll >= opponent.armcla:
+          damagedealt = player.special_attack()
+          opponent.take_damage(damagedealt)
+          print(f"You dealt {damagedealt} to {opponent.name}")
+          print(f"{opponent.name} has {opponent.health} HP left")
+          time.sleep(1)
+          baddie_turn(player, opponent)
+        else:
+          print("Oh no, you missed!")
+          time.sleep(1)
+          baddie_turn(player, opponent)
     else:
-      print("Oh no, you missed!")
+      if player.charges == 0:
+        print("You are out of charges!")
+        player.charges -= 1
+      input("Press enter to launch a normal attack")
       time.sleep(1)
-      baddie_turn(player, opponent)
+      attackroll = player.atk_roll()
+      print(f"You rolled {attackroll} to strike again {opponent.name} armour class of {opponent.armcla}!")
+      if attackroll >= opponent.armcla:
+        damagedealt = player.deal_damage()
+        opponent.take_damage(damagedealt)
+        print(f"You dealt {damagedealt} to {opponent.name}")
+        print(f"{opponent.name} has {opponent.health} HP left")
+        time.sleep(1)
+        baddie_turn(player, opponent)
+      else:
+        print("Oh no, you missed!")
+        time.sleep(1)
+        baddie_turn(player, opponent)
   else:
       print("Game over, you lose!")
       time.sleep(2)
